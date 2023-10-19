4b1b858f61fe7f258066389620ab6ff6cf55dd82
Tyuiu.KoevaSJ@outlook.com <koevasonya_0902@mail.ru>
2023-10-20 00:00:58 +0500

    Task1

---
+++ Tyuiu.KoevaSJ.Sprint3.Task1.V30.Lib/Dataservice.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.KoevaSJ.Sprint3.Task1.V30.Lib
+{
+    public class DataService : ISprint3Task1V30
+    {
+        public double GetSumSeries(double value, int startValue, int stopValue)
+        {
+           double sumSeries = 0;
+           int k = startValue;
+          
+           while (startValue <= stopValue)
+            {
+                sumSeries = sumSeries + ((Math.Pow(value, k) + (5 / (k + 4))) * Math.Sin(value));
+                startValue++;
+            }
+            return Math.Round(sumSeries, 3);
+        }
+    }
+}
---
+++ Tyuiu.KoevaSJ.Sprint3.Task1.V30.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KoevaSJ.Sprint3.Task1.V30.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KoevaSJ.Sprint3.Task1.V30.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4687dfde-568b-41a7-80b5-37c4195944fa")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
