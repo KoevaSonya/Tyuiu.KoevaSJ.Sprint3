724927b2044b75ecd2ff7344096a68e78f0d6828
Tyuiu.KoevaSJ@outlook.com <koevasonya_0902@mail.ru>
2023-10-29 12:33:24 +0500

    Task5

---
+++ Tyuiu.KoevaSJ.Sprint3.Task5.V3.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KoevaSJ.Sprint3.Task5.V3.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KoevaSJ.Sprint3.Task5.V3.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("34c27676-6c07-435b-bf75-4a44ce4300f5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
